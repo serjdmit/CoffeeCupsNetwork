1e71a9f89766d00d1b4fb329a35f307ca19444e0
Sergei Dmitrijev <mr.dmit@gmail.com>
2020-01-08 15:34:44 +0200

    Fixed forbidden requirements and created image uploading function

--- coffee_cups_functions/functions/index.js
+++ coffee_cups_functions/functions/index.js
@@ -3,7 +3,7 @@ const functions = require("firebase-functions");
 const app = require("express")();
 
 const { getAllCups, postOneCup } = require("./handlers/cups");
-const { signup, login } = require("./handlers/users");
+const { signup, login, uploadImage } = require("./handlers/users");
 
 const FBAuth = require("./util/fbAuth");
 
@@ -14,5 +14,6 @@ app.post("/cup", FBAuth, postOneCup);
 //Users routes
 app.post("/signup", signup);
 app.post("/login", login);
+app.post("/user/image", FBAuth, uploadImage);
 
 exports.api = functions.https.onRequest(app);
--- coffee_cups_functions/functions/util/admin.js
+++ coffee_cups_functions/functions/util/admin.js
@@ -10,7 +10,15 @@ var serviceAccount = require("../../social_app_key.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://socialapp-959ec.firebaseapp.com"
+    databaseURL: "https://socialapp-959ec.firebaseapp.com",
+    storageBucket: "socialapp-959ec.appspot.com",
+    apiKey: "AIzaSyDQdOBL7ltN1ZSs469c9rqhs42pufMW3SY",
+    authDomain: "socialapp-959ec.firebaseapp.com",
+    databaseURL: "https://socialapp-959ec.firebaseio.com",
+    projectId: "socialapp-959ec",
+    messagingSenderId: "317894950810",
+    appId: "1:317894950810:web:79796f2bacdf7a8924c423",
+    measurementId: "G-GTB0W5X93B"
 });
 
 const db = admin.firestore();
