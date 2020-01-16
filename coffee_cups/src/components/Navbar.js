4a673b1aceb1fb011c96f310291ff7a58233e9f0
Sergei Dmitrijev <mr.dmit@gmail.com>
2020-01-16 02:56:54 +0200

    Started to develope frontend part on React

---
+++ coffee_cups/src/components/Navbar.js
@@ -0,0 +1,26 @@
+// MUI stuff
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import Toolbar from "@material-ui/core/Toolbar";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <AppBar position="fixed">
+            <Toolbar className="nav-container">
+                <Button color="inherit" component={Link} to="/">
+                    Home
+                </Button>
+                <Button color="inherit" component={Link} to="/login">
+                    Login
+                </Button>
+                <Button color="inherit" component={Link} to="/signup">
+                    Signup
+                </Button>
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default Navbar;
---
+++ coffee_cups/src/pages/home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const home = () => {
+    return <div>home page</div>;
+};
+
+export default home;
